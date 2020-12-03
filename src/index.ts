64cfea803dd8014f3629fbe44c94587c1270c83b
Anand Chowdhary <mail@anandchowdhary.com>
2020-12-03 18:27:36 +0530

    :sparkles: Generate graphs for each duration

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,37 @@
 import { Octokit } from "@octokit/rest";
 import slugify from "@sindresorhus/slugify";
 import { CanvasRenderService } from "chartjs-node-canvas";
+import dayjs from "dayjs";
 import { ensureDir, readFile, readJson, writeFile, writeJson } from "fs-extra";
 import { safeLoad } from "js-yaml";
 import { join } from "path";
 
 const canvasRenderService = new CanvasRenderService(600, 400);
+
+/** Get commits for a history file */
+const getHistoryItems = async (
+  octokit: Octokit,
+  owner: string,
+  repo: string,
+  slug: string,
+  page: number
+) => {
+  console.log("Fetching history - page", 1);
+  const results = await octokit.repos.listCommits({
+    owner,
+    repo,
+    path: `history/${slug}.yml`,
+    per_page: 100,
+    page,
+  });
+  let data = results.data;
+  if (
+    data.length === 100 &&
+    !dayjs(data[0].commit.author.date).isBefore(dayjs().subtract(1, "year"))
+  )
+    data.push(...(await getHistoryItems(octokit, owner, repo, slug, page + 1)));
+  return data;
+};
 
 const getUptimeColor = (uptime: number) =>
   uptime > 95
@@ -157,58 +183,115 @@ export const generateGraphs = async () => {
       color: getResponseTimeColor(timeYear),
     });
 
-    const history = await octokit.repos.listCommits({
-      owner,
-      repo,
-      path: `history/${slug}.yml`,
-      per_page: 10,
-    });
-    if (!history.data.length) continue;
-    const data: [number, string][] = history.data
-      .reverse()
+    const items = await getHistoryItems(octokit, owner, repo, slug, 1);
+    const responseTimes: [string, number][] = items
       .filter(
         (item) =>
           item.commit.message.includes(" in ") &&
-          Number(item.commit.message.split(" in ")[1].split("ms")[0]) !== 0
+          Number(item.commit.message.split(" in ")[1].split("ms")[0].trim()) !== 0 &&
+          !isNaN(Number(item.commit.message.split(" in ")[1].split("ms")[0].trim()))
       )
-      .map((item) => [
-        Number(item.commit.message.split(" in ")[1].split("ms")[0]),
-        String(item.commit.author.date),
-      ]);
-    const image = await canvasRenderService.renderToBuffer({
-      type: "line",
-      data: {
-        labels: [1, ...data.map((item) => item[1])],
-        datasets: [
-          {
-            backgroundColor: "#89e0cf",
-            borderColor: "#1abc9c",
-            data: [1, ...data.map((item) => item[0])],
+      /**
+       * Parse the commit message
+       * @example "🟥 Broken Site is down (500 in 321 ms) [skip ci] [upptime]"
+       * @returns [Date, 321] where Date is the commit date
+       */
+      .map(
+        (item) =>
+          [
+            item.commit.author.date,
+            parseInt(item.commit.message.split(" in ")[1].split("ms")[0].trim()),
+          ] as [string, number]
+      )
+      .filter((item) => item[1] && !isNaN(item[1]));
+
+    const tDay = responseTimes.filter((i) => dayjs(i[0]).isAfter(dayjs().subtract(1, "day")));
+    const tWeek = responseTimes.filter((i) => dayjs(i[0]).isAfter(dayjs().subtract(1, "week")));
+    const tMonth = responseTimes.filter((i) => dayjs(i[0]).isAfter(dayjs().subtract(1, "month")));
+    const tYear = responseTimes.filter((i) => dayjs(i[0]).isAfter(dayjs().subtract(1, "year")));
+    const dataItems: [string, [string, number][]][] = [
+      [`${slug}/response-time-day.png`, tDay],
+      [`${slug}/response-time-week.png`, tWeek],
+      [`${slug}/response-time-month.png`, tMonth],
+      [`${slug}/response-time-year.png`, tYear],
+    ];
+
+    for await (const dataItem of dataItems) {
+      await writeFile(
+        join(".", "graphs", dataItem[0]),
+        await canvasRenderService.renderToBuffer({
+          type: "line",
+          data: {
+            labels: [1, ...responseTimes.map((item) => item[0])],
+            datasets: [
+              {
+                backgroundColor: "#89e0cf",
+                borderColor: "#1abc9c",
+                data: [1, ...responseTimes.map((item) => item[1])],
+              },
+            ],
           },
-        ],
-      },
-      options: {
-        legend: { display: false },
-        scales: {
-          xAxes: [
-            {
-              display: false,
-              gridLines: {
-                display: false,
-              },
+          options: {
+            legend: { display: false },
+            scales: {
+              xAxes: [
+                {
+                  display: false,
+                  gridLines: {
+                    display: false,
+                  },
+                },
+              ],
+              yAxes: [
+                {
+                  display: false,
+                  gridLines: {
+                    display: false,
+                  },
+                },
+              ],
             },
-          ],
-          yAxes: [
+          },
+        })
+      );
+    }
+
+    await writeFile(
+      join(".", "graphs", slug, "response-time.png"),
+      await canvasRenderService.renderToBuffer({
+        type: "line",
+        data: {
+          labels: [1, ...responseTimes.map((item) => item[0])],
+          datasets: [
             {
-              display: false,
-              gridLines: {
-                display: false,
-              },
+              backgroundColor: "#89e0cf",
+              borderColor: "#1abc9c",
+              data: [1, ...responseTimes.map((item) => item[1])],
             },
           ],
         },
-      },
-    });
-    await writeFile(join(".", "graphs", `${slug}.png`), image);
+        options: {
+          legend: { display: false },
+          scales: {
+            xAxes: [
+              {
+                display: false,
+                gridLines: {
+                  display: false,
+                },
+              },
+            ],
+            yAxes: [
+              {
+                display: false,
+                gridLines: {
+                  display: false,
+                },
+              },
+            ],
+          },
+        },
+      })
+    );
   }
 };
