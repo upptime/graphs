caf080567dd64328f51f2f752ca774c809a6720e
Anand Chowdhary <github@anandchowdhary.com>
2026-05-28 10:02:46 +0000

    :construction_worker: Use npm trusted publishing

--- release.config.js
+++ release.config.js
@@ -1,1 +1,32 @@
-module.exports = require("@koj/config").releaseMaster;
+const { releaseMaster } = require("@koj/config");
+
+module.exports = {
+  ...releaseMaster,
+  plugins: releaseMaster.plugins.map((plugin) => {
+    const name = Array.isArray(plugin) ? plugin[0] : plugin;
+    const options = Array.isArray(plugin) ? plugin[1] || {} : {};
+
+    if (name === "@semantic-release/github") {
+      return [
+        "@semantic-release/github",
+        {
+          ...options,
+          failComment: false,
+          successComment: false,
+        },
+      ];
+    }
+
+    if (name === "@semantic-release/npm") {
+      return [
+        "@semantic-release/npm",
+        {
+          ...options,
+          npmPublish: true,
+        },
+      ];
+    }
+
+    return plugin;
+  }),
+};
